ac1f82fdb37c179794ebab45fc44048a9a799a9b
陈绪明 <1207314598@qq.com>
2019-07-09 17:14:58 +0800

    [陈绪明]新建了三个StudentCourse类，在StudentCourse.java类中编写了一部分代码

---
+++ src/main/java/com/briup/apps/sms/bean/StudentCourse.java
@@ -0,0 +1,9 @@
+package com.briup.apps.sms.bean;
+
+public class StudentCourse {
+     private Long id;
+     private Long user_id;
+     private Long course_id;
+     private Long xk_time;
+     private String grade;
+}
