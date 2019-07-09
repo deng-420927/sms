544ee98c59eaef56072773f22da7a564cef89318
黄佳军 <1604799954qq.com>
2019-07-09 17:00:37 +0800

    [黄佳军]新建course部分代码

---
+++ src/main/java/com/briup/apps/sms/bean/Course.java
@@ -0,0 +1,33 @@
+package com.briup.apps.sms.bean;
+
+public class Course {
+    private long id;
+    private String name;
+    private double credit;
+    private long user_id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getCredit() {
+		return credit;
+	}
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+    
+}
